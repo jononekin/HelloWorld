0495d3d8cb7731ec56f3fb9f438c497dd1fe1f35
jon <jon.iturrioz@ehu.es>
2024-02-27 11:56:15 +0100

    dataAcess added

---
+++ HelloWorld/src/dataAccess/DataAccess.java
@@ -0,0 +1,11 @@
+package dataAccess;
+
+public class DataAccess {
+	List<String> getTexts(){
+		return null; //hemen datubase atzipena inplementatu
+	}
+	HashMap<String,String> getTraductions(String msg){
+		return null; //hemen datubase atzipena inplementatu
+	}
+
+}
