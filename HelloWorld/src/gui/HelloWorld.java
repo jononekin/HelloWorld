c7f7daa04239f8c763ec69710163ebcd3546a60c
jon <jon.iturrioz@ehu.es>
2024-02-27 11:08:19 +0100

    Message klasea gehituta + HelloWorld aldatuta

--- HelloWorld/src/gui/HelloWorld.java
+++ HelloWorld/src/gui/HelloWorld.java
@@ -7,6 +7,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import domain.Message;
+
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.util.HashMap;
@@ -22,7 +24,12 @@ public class HelloWorld extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					HelloWorld frame = new HelloWorld();
+					HashMap<String,String> hm=new HashMap<String,String>();
+					hm.put("eu", "Kaixo Mundua");
+					hm.put("es", "Hola Mundo");
+					Message m=new Message ("hello world",hm);
+					HelloWorld frame = new HelloWorld(m, "eu");
+
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -34,7 +41,7 @@ public class HelloWorld extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public HelloWorld() {
+	public HelloWorld(Message m, String lang)   {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -42,7 +49,7 @@ public class HelloWorld extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblHelloWorld = new JLabel("Kaixo Mundua");
+		JLabel lblHelloWorld = new JLabel(m.getTraductions(lang));
 		lblHelloWorld.setFont(new Font("Lucida Grande", Font.PLAIN, 30));
 		lblHelloWorld.setBounds(115, 93, 225, 44);
 		contentPane.add(lblHelloWorld);
